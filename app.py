cc0f228044a6ac2c3f47d8493a1a596f4e1a2deb
meIhdin <121300415+meIhdin@users.noreply.github.com>
2024-08-31 03:59:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-def addition(a,b):
-    return a+b
+def addition(a,b,c):
+    return a+b+c
 
 def sustraction(a,b):
     return a-b
 
-def division(a,b):
-    return a/b
+def division(a,b,c):
+    return a/b/c
